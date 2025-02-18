c81c2f09c8aa9e6a3a342e5708278b329a061e76
Ayush Kant <142834568+Ayushkant3011@users.noreply.github.com>
2025-02-18 18:22:52 +0530

    Pages(Home, User, Captain, Signup, Login) created with more accuracy

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import UserLogin from './pages/UserLogin';
+import UserSignup from './pages/UserSignup';
+import CaptainLogin from './pages/CaptainLogin';
+import CaptainSignup from './pages/CaptainSignup';
 
 const App = () => {
   return (
-    <div className='bg-red-300'>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+      </Routes>
+    </div>
   )
 }
 
