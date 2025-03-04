0815e1d323e802aebf5ea25d1ac097a2e7e024e7
Ayush Kant <142834568+Ayushkant3011@users.noreply.github.com>
2025-03-04 16:08:11 +0530

    autoCompleteSuggestions API created

--- Backend/Controllers/Maps.Controller.js
+++ Backend/Controllers/Maps.Controller.js
@@ -30,9 +30,28 @@ module.exports.getDistanceTime = async (req,res,next)=>{
         const distanceTime = await mapService.getDistanceTime(origin, destination);
 
         res.status(200).json(distanceTime);
-        
+
     } catch(err){
         console.error(err);
         res.status(500).json({ message: "Internal Server Error"});
+    }
+}
+
+module.exports.getAutocompleteSuggestions = async (req, res, next) => {
+    try{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const {input} = req.query;
+
+        const suggestions = await mapService.getAutocompleteSuggestions(input);
+
+        res.status(200).json(suggestions);
+        
+    } catch(error){
+        console.error(error);
+        res.status(500).json({message: 'Internal Server Error'});
     }
 }
--- Backend/Services/Maps.service.js
+++ Backend/Services/Maps.service.js
@@ -48,3 +48,25 @@ module.exports.getDistanceTime = async (origin, destination) =>{
         throw err;
     }
 }
+
+module.exports.getAutocompleteSuggestions = async (input) =>{
+    if(!input){
+        throw new Error('Query is required');
+    }
+
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+    const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${apiKey}`;
+
+    try{
+        const response = await axios.get(url);
+        if(response.data.status === 'OK'){
+            return response.data.predictions;
+        } else{
+            throw new Error('Unable to find suggestions');  
+        }
+
+    } catch(err){
+        console.error(err);
+        throw err;
+    }
+}
