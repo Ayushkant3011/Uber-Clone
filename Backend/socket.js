a41372bbe3919d625cd27b062dc3f7baffb27415
Ayush Kant <142834568+Ayushkant3011@users.noreply.github.com>
2025-04-16 20:48:19 +0530

    captain location fetched

--- Backend/socket.js
+++ Backend/socket.js
@@ -29,6 +29,21 @@ const initializeSocket = (server) => {
             }
         });
 
+        socket.on('update-location-captain', async (data) => {
+            const {userId, location} = data;
+
+            if (!location || location.ltd || location.lng) {
+                return socket.emit('error', { message: 'Invalid location data' });
+            }
+
+            await captainModel.findByIdAndUpdate(userId, {
+                location:{
+                    ltd: location.ltd,
+                    lng: location.lng
+                }
+            });
+        })
+
 
         socket.on('disconnect', () => {
             console.log(`Socket disconnected: ${socket.id}`);
