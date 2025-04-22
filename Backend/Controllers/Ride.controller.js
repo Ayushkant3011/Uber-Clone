d5419038e52f336cc165d766cfdb0fc5dea94f75
Ayush Kant <142834568+Ayushkant3011@users.noreply.github.com>
2025-04-22 20:31:03 +0530

    RideController and RideService updated/ ConfirmRide Created

--- Backend/Controllers/Ride.controller.js
+++ Backend/Controllers/Ride.controller.js
@@ -65,3 +65,28 @@ module.exports.getFare = async (req, res) => {
         return res.status(500).json({message: err.message});
     }
 }
+
+
+module.exports.confirmRide = async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {rideId} = req.body;
+
+    try{
+        const ride = await rideService.confirmRide(rideId, req.captain._id);
+        
+        sendMessageToSocketId(ride.user.socketId,{
+            event:'ride-confimred',
+            data: ride
+        })
+        
+        
+        res.status(200).json(ride);
+
+    } catch(err){
+        return res.status(500).json({message: err.message});
+    }
+}
--- Backend/Services/Ride.service.js
+++ Backend/Services/Ride.service.js
@@ -69,3 +69,29 @@ module.exports.createRide = async ({
     return ride;
 }
 
+
+
+module.exports.confirmRide = async ({
+    rideId
+}) => {
+    if(!rideId){
+        throw new Error('Ride ID is required');
+    }
+
+    await rideModel.findOneAndUpdate({
+        _id: rideId
+    },{
+        status:'accepted',
+        captain: captain._id
+    })
+
+    const ride = await rideModel.findOne({
+        _id:ride
+    }).populate('user');
+
+    if(!ride){
+        throw new Error('Ride not Found');
+    }
+    
+    return ride;
+}
